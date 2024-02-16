5c5ced49bdbcc293f7a1d00a44f14bf25ed303bb
Abdullah Qaisar <abdullahqaisarr@gmail.com>
2024-02-16 15:34:01 +0500

    Add getProduct endpoint to ProductsController

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -6,7 +6,7 @@ export class ProductsService {
   products: Product[] = [];
 
   insertProduct(title: string, description: string, price: number) {
-    const prodId = new Date().toDateString();
+    const prodId = Math.random().toString();
     const product = new Product(prodId, title, description, price);
     this.products.push(product);
     return prodId;
@@ -14,5 +14,14 @@ export class ProductsService {
 
   getProducts() {
     return [...this.products];
+  }
+
+  getSingleProduct(productId: string) {
+    const product = this.products.find((prod) => prod.id === productId);
+
+    if (!product) {
+      throw new NotFoundException('Could not find product');
+    }
+    return { ...product };
   }
 }
